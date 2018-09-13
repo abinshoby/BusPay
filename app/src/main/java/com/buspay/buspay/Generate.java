fab0ae1eaf22c933d6757dda014cc8124182e822
abin <abinshoby@gmail.com>
2018-09-13 17:04:27 +0530

    qr module

---
+++ app/src/main/java/com/buspay/buspay/Generate.java
@@ -0,0 +1,35 @@
+package com.buspay.buspay;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import java.util.Calendar;
+
+public class Generate extends Activity {
+ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generate);
+        imageView=findViewById(R.id.imageView);
+        String text= Calendar.getInstance().getTime().toString(); // Whatever you need to encode in the QR code
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try {
+            BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE,200,200);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            imageView.setImageBitmap(bitmap);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- app/src/main/java/com/buspay/buspay/QR.java
+++ app/src/main/java/com/buspay/buspay/QR.java
@@ -1,52 +1,45 @@
 package com.buspay.buspay;
 
-import android.Manifest;
-import android.net.Uri;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
+
+
+
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
-import com.google.android.gms.vision.barcode.Barcode;
-import com.google.android.gms.vision.barcode.BarcodeDetector;
+public class QR extends Activity implements View.OnClickListener {
 
-public class QR extends AppCompatActivity {
-    private static final String LOG_TAG = "Barcode Scanner API";
-    private static final int PHOTO_REQUEST = 10;
-    private TextView scanResults;
-    private BarcodeDetector detector;
-    private Uri imageUri;
-    private static final int REQUEST_WRITE_PERMISSION = 20;
-    private static final String SAVED_INSTANCE_URI = "uri";
-    private static final String SAVED_INSTANCE_RESULT = "result";
+    Button btnTakePicture, btnScanBarcode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr);
+        initViews();
     }
-    Button button = (Button) findViewById(R.id.button);
-    TextView scanResults = (TextView) findViewById(R.id.scan_results);
-        if (savedInstanceState != null) {
-        imageUri = Uri.parse(savedInstanceState.getString(SAVED_INSTANCE_URI));
-        scanResults.setText(savedInstanceState.getString(SAVED_INSTANCE_RESULT));
+
+    private void initViews() {
+        btnTakePicture = findViewById(R.id.btnTakePicture);
+        btnScanBarcode = findViewById(R.id.btnScanBarcode);
+        btnTakePicture.setOnClickListener(this);
+        btnScanBarcode.setOnClickListener(this);
     }
-        button.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            ActivityCompat.requestPermissions(MainActivity.this, new
-                    String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_WRITE_PERMISSION);
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.btnTakePicture:
+                Intent i= new Intent(QR.this, Generate.class);
+                startActivity(i);
+                break;
+            case R.id.btnScanBarcode:
+                Intent j=new Intent(QR.this, ScannedBarcodeActivity.class);
+                startActivity(j);
+                break;
         }
-    });
 
-    detector = new BarcodeDetector.Builder(getApplicationContext())
-            .setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE)
-                .build();
-        if (!detector.isOperational()) {
-        scanResults.setText("Could not set up the detector!");
-        return;
     }
-
 }
